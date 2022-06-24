a653bd56984186aabafbeead2d83d079eae61c08
Inga <inga.lingiene12@gmail.com>
2022-06-24 19:01:52 +0300

    back com css

--- my-react/src/Components/Back/ManikiuroListoAtvaizdavimas.jsx
+++ my-react/src/Components/Back/ManikiuroListoAtvaizdavimas.jsx
@@ -11,21 +11,22 @@ function ManikiuroListoAtvaizdavimas({manikiuras, setIstrintiId, setRedaguotiMod
                     </div>
                     <div className="mani-listas">
                         <button type="button" className="manikiuro-buttons redaguoti" onClick={()=>setRedaguotiModalData(manikiuras)}>Redaguoti</button>{/*7.Modalo iskvietimas*/}
-                        <button type="button" className="manikiuro-buttons istrinti" onClick={()=>setIstrintiId({id:manikiuras.id})}>Istrinti</button> {/*////6.Istrinimo mygtukas is ManikiuroListoAtvaizdavimas.jsx kuris istrins visa jo info*/}
+                        <button type="button" className="manikiuro-buttons istrinti" onClick={()=>setIstrintiId({id:manikiuras.id})}>Ištrinti</button> {/*////6.Istrinimo mygtukas is ManikiuroListoAtvaizdavimas.jsx kuris istrins visa jo info*/}
                     </div>
                 </div>
-                <ul className="tree-group">{/*40004 komentaro idejimas*/}
-            {                       
-            manikiuras.comments ? manikiuras.comments.slice(0, -5).split('-^o^-,').map((c, i) => (
-                <li className="list-group-item" key={i}>
-                    {c} {/*c-komentaras*/}{/*tree.cid.-komentaru id*/}
-                        <div>
-                            <button type="button" onClick={() => deleteComment(manikiuras.cid.split(',')[i])} className="btn btn-outline-danger mt-3 deletle">Delete</button>{/*700 komentaro istrinimas*/}
-                         </div>
-                </li>)) : null
+                <ul className="back-komentarai">{/*40004 komentaro idejimas*/}
+                <h3>Komentarai</h3>
+                   {                       
+                        manikiuras.comments ? manikiuras.comments.slice(0, -5).split('-^o^-,').map((c, i) => (
+                            
+                            <li className="back-komentaras" key={i}>{c} {/*c-komentaras*/}{/*tree.cid.-komentaru id*/}
+                            <div>
+                                <button type="button" className="manikiuro-buttons istrinti trint" onClick={() => deleteComment(manikiuras.cid.split(',')[i])}>Ištrinti</button>{/*700 komentaro istrinimas*/}
+                            </div>
+                            </li>)) : null
                                  ////40004//slice(0, -5).split('-^o^-,') atemem is app.jx komentaro gala ta katina//c-komentarai, i- indeksai
-            }
-        </ul>
+                    }
+                </ul>
             </li>
         )
     }
